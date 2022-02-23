feeb5e5be88a78a19ae6e2e6d408c97ea0f7b00e
Jonas Wallmann <jonas.wallmann@code.berlin>
2022-02-23 10:02:14 +0100

    Initial Commit

---
+++ Music Lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Lesson
+//
+//  Created by Jonas Wallmann on 23.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Lesson/Music_LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_LessonApp.swift
+//  Music Lesson
+//
+//  Created by Jonas Wallmann on 23.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Music_LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
